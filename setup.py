875a5331f663d22b57048130fd2550102ee573b7
Apostolos Georgiadis <apostolos.georgiadis@gmx.ch>
2018-12-11 13:14:15 +0100

    version 0.0.4 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="nfiniity_utils",
-    version="0.0.2",
+    version="0.0.4",
     author="Apostolos Georgiadis",
     author_email="apostolos.georgiadis@nfiniity.com",
     description=" Diverse python utils by nfiniity GmbH",
