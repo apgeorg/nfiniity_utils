361df52d207658be9e50e34e8b869acd8944910a
Apostolos Georgiadis <apostolos.georgiadis@gmx.ch>
2019-01-03 08:45:44 +0100

    some changes in signal

--- nfiniity_utils/__init__.py
+++ nfiniity_utils/__init__.py
@@ -1,3 +1,4 @@
 from .timer import Timer
+from .signal import Lowpass
 
 __version__="0.0.6"
--- nfiniity_utils/signal.py
+++ nfiniity_utils/signal.py
@@ -2,12 +2,9 @@
 class Lowpass(object):
     def __init__(self, tau, ts):
         self._a = 1. / (tau / ts + 1.)
-        self._b = tau / ts / (tau / ts + 1.);
+        self._b = tau / ts / (tau / ts + 1.)
         self._last_value = 0.
         self._ready = False
-
-    def get(self):
-        return self._last_value
 
     def filter(self, val):
         if self._ready:
@@ -16,3 +13,7 @@ class Lowpass(object):
             self._ready = True
         self._last_value = val
         return val
+
+    @property
+    def last_value(self):
+        return self._last_value
